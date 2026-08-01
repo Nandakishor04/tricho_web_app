ff37e8d0f419c4b68377bf966f769e841e11c981
Nandakishor04 <kukuntlanani123@gmail.com>
2026-08-01 14:15:47 +0530

    Add tricholens/tricholensApp.swift

---
+++ tricholens/tricholensApp.swift
@@ -0,0 +1,17 @@
+//
+//  tricholensApp.swift
+//  tricholens
+//
+//  Created by SAIL on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct tricholensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
